2f9d9ecbe1ab6d7fbd61ad9d046c0a83dfb19759
Sebastiano Armeli <seba.armeli@gmail.com>
2024-02-17 16:14:31 -0800

    Upgrade ParticicleBackground

--- src/components/ParticlesBackground.js
+++ src/components/ParticlesBackground.js
@@ -1,25 +1,45 @@
-import { useCallback } from "react";
-import Particles from "react-tsparticles";
+//import { useCallback } from "react";
+import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadFull } from "tsparticles";
 
 const ParticlesBackground = () => {
-  const particlesInit = useCallback(async (engine) => {
-    console.log(engine);
-    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      await loadFull(engine);
+      //await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
 
-  const particlesLoaded = useCallback(async (container) => {
-    await console.log(container);
-  }, []);
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+  // const particlesInit = useCallback(async (engine) => {
+  //   console.log(engine);
+  //   // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+  //   // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+  //   // starting from v2 you can add only the features you need reducing the bundle size
+  //   await loadFull(engine);
+  // }, []);
+
+  // const particlesLoaded = useCallback(async (container) => {
+  //   await console.log(container);
+  // }, []);
 
   return (
     <Particles
       id="particles-box"
       className="particles"
-      init={particlesInit}
       loaded={particlesLoaded}
       options={{
         fullScreen: {
